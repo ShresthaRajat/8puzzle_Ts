1f35b29bbf78db512fea19ef6410a713cad1dace
Rajat Shrestha <shrestharajat@hotmail.com>
2020-12-06 11:16:08 +0545

    Add queue class

---
+++ queue.ts
@@ -0,0 +1,37 @@
+interface IQueue<T> {
+    enqueue(item: T): void;
+    dequeue(): T;
+    size(): number;
+}
+
+class Queue<T> implements IQueue<T> {
+    private storage: T[] = [];
+
+    constructor(private capacity: number = Infinity) { }
+
+    enqueue(item: T): void {
+        if (this.size() === this.capacity) {
+            throw Error("Queue has reached max capacity, you cannot add more items");
+        }
+        this.storage.push(item);
+    }
+    dequeue(): T | undefined {
+        return this.storage.shift();
+    }
+    size(): number {
+        return this.storage.length;
+    }
+}
+
+// burrowed from: https://dev.to/glebirovich/typescript-data-structures-stack-and-queue-hld#queue
+
+// Examples:
+
+// const queue = new Queue<string>();
+
+// queue.enqueue("A");
+// queue.enqueue("B");
+
+// console.log(queue.size());    // Output: 2
+// console.log(queue.dequeue()); // Output: "A"
+// console.log(queue.size());    // Output: 1
